497d86c2e0dff1d49d6b080a9ad6438bfc094f82
politicalCalculator <kyle.mlynarski@mcvts.org>
2017-02-24 21:58:43 -0500

    more tests

--- runner.php
+++ runner.php
@@ -1,6 +1,4 @@
 <?php
-$file = fopen("ip.txt","a");
-$ip=$_SERVER['REMOTE_ADDR'];
-echo fwrite($file,$ip);
-fclose($file);
-?> 
+$line = date('Y-m-d H:i:s') . " - $_SERVER[REMOTE_ADDR]";
+file_put_contents('visitors.log', $line . PHP_EOL, FILE_APPEND);
+?>
