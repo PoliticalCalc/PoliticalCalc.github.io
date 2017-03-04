aeb77bc8f0ffef9137c6e976fb16591033daf7b1
politicalCalculator <kyle.mlynarski@mcvts.org>
2017-03-03 20:59:31 -0500

    Sets up the form
    Form now links to the runner.php file

---
+++ runner.php
@@ -0,0 +1,4 @@
+<?php
+//This File is the PHP runner, which means it will take the data from the form questions for processing
+//The file header is established here so it is ready for someone to input code directly now
+?>
